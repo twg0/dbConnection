76a4e31d1300bd33043370a7f71d4b32c563d72c
twg0 <fruitsjh@gmail.com>
2023-05-22 15:04:22 +0900

    feat : UserTable CRUD 구현

---
+++ src/main/java/com/example/springboot/db/ConnectionChecker.java
@@ -0,0 +1,76 @@
+package com.example.springboot.db;
+
+import java.sql.*;
+
+public class ConnectionChecker {
+
+    private static Connection connection;
+    private static Statement stmt;
+
+    private ConnectionChecker(){}
+
+    public static Connection getConn() throws SQLException, ClassNotFoundException {
+        if(connection == null) {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conn = DriverManager.getConnection(
+                    "jdbc:mysql://127.0.0.1:3306/spring-db", "root", "1234");
+            stmt = conn.createStatement();
+        }
+        return connection;
+    }
+
+    public static void select() throws SQLException {
+        ResultSet rs = stmt.executeQuery("SELECT * FROM `spring-db`.`user`");
+        while(rs.next()) {
+            String line = rs.getString(1);
+            String line2 = rs.getString(2);
+            String line3 = rs.getString(3);
+            System.out.println(line + " " + line2 + " " + line3);
+        }
+    }
+
+    public static void createUserTable() throws SQLException {
+        stmt.execute("CREATE TABLE `spring-db`.`user` (" +
+                "  `id` INT NOT NULL AUTO_INCREMENT," +
+                "  `name` VARCHAR(45) NOT NULL," +
+                "  `phone` VARCHAR(45) NOT NULL," +
+                "  PRIMARY KEY (`id`));");
+        System.out.println("create UserTable");
+    }
+
+    public static void createUser(String userName, String phone) throws SQLException {
+        stmt.execute("Insert Into `spring-db`.`user`(name,phone) VALUES('" + userName + "','" + phone + "')");
+        System.out.println("create user");
+    }
+
+    public static void updateUserName(int id, String name) throws SQLException {
+        stmt.execute("UPDATE `spring-db`.`user` SET name = '" + name + "' WHERE id = '" + id + "';");
+    }
+
+    public static void dropUserTable() throws SQLException {
+        stmt.execute("DROP TABLE `spring-db`.`user`");
+    }
+    public static void deleteAllUser() throws SQLException {
+        stmt.execute("DELETE FROM `spring-db`.`user`");
+    }
+
+    public static ResultSet getUsers() throws SQLException {
+        return stmt.executeQuery("SELECT name FROM `spring-db`.`user`");
+    }
+
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
+//        ConnectionChecker.deleteAllUser();
+        ConnectionChecker.getConn();
+        ConnectionChecker.dropUserTable();
+        ConnectionChecker.createUserTable();
+
+        ConnectionChecker.createUser("kyeongrok","1577");
+        ConnectionChecker.createUser("Ronaldo", "1544");
+        ConnectionChecker.createUser("Messi","1566");
+        ConnectionChecker.createUser("Dukbae","1588");
+        ConnectionChecker.updateUserName(2,"newUserName");
+
+        ConnectionChecker.select();
+    }
+}
